94ff2a1b4c2633cb6a32fe891c43b772fa3cb7c3
Siarhei Sadouski <s.sadovski.z@gmail.com>
2022-11-08 16:52:22 +0400

    fix: removed queue defining to regular use

--- src/generic_actor.cr
+++ src/generic_actor.cr
@@ -6,12 +6,23 @@ module GenericActor
   private abstract struct Message
   end
 
-  alias MessageQueue = Channel(Message)
-
   @message_loop_started = Atomic::Flag.new
+  @regular_message_queue = Channel(Message).new(100)
+  @priority_message_queue = Channel(Message).new(100)
+
+  private def actor_handle(message : Message) : Nil
+    nil
+  end
 
   private def actor_loop
-    raise "Unimplemented any call"
+    loop do
+      select
+      when message = @priority_message_queue.receive
+        actor_handle(message)
+      when message = @regular_message_queue.receive
+        actor_handle(message)
+      end
+    end
   end
 
   private def check_message_loop
@@ -19,60 +30,20 @@ module GenericActor
     spawn { actor_loop }
   end
 
-  private def actor_handle(message : Message) : Nil
-    nil
-  end
-
   macro cast_def(name, args, &block)
-    define_regular_queue
     define_cast_def @regular_message_queue, {{name}}, {{args}}, {{ block }}
   end
 
   macro prioritized_cast_def(name, args, &block)
-    define_prioritized_queue
     define_cast_def @priority_message_queue, {{name}}, {{args}}, {{ block }}
   end
 
   macro call_def(name, args, result, &block)
-    define_regular_queue
     define_call_def @regular_message_queue, {{name}}, {{args}}, {{result}}, {{ block }}
   end
 
   macro prioritized_call_def(name, args, result, &block)
-    define_prioritized_queue
     define_call_def @priority_message_queue, {{name}}, {{args}}, {{result}}, {{ block }}
-  end
-
-  private macro define_actor_loop
-    private def actor_loop
-      loop do
-        select
-        {% if @type.has_constant?("PRIORITIZED_QUEUE_DEFINED") %}
-        when priority_message = @priority_message_queue.receive
-          actor_handle(priority_message) {% end %}
-        {% if @type.has_constant?("REGULAR_QUEUE_DEFINED") %}
-        when regular_message = @regular_message_queue.receive
-          actor_handle(regular_message) {% end %}
-        end
-      end
-    end
-  end
-
-  private macro define_prioritized_queue
-    {% if !@type.has_constant?("PRIORITIZED_QUEUE_DEFINED") %}
-      @priority_message_queue = MessageQueue.new(100)
-      PRIORITIZED_QUEUE_DEFINED = true
-      define_actor_loop
-    {% end %}
-  end
-
-  # regular_message_queue defining if not defined regular_queue
-  private macro define_regular_queue
-    {% if !@type.has_constant?("REGULAR_QUEUE_DEFINED") %}
-      @regular_message_queue = MessageQueue.new(100)
-      REGULAR_QUEUE_DEFINED = true
-      define_actor_loop
-    {% end %}
   end
 
   # define message with unique type according method name and his args
